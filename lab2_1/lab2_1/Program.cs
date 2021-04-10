28e9eacc5e4a5d1ff63575896c2c568f4e84c275
Froraut <iliaborovoy2002@gmail.com>
2021-04-10 10:57:18 +0300

    Lab2cs

--- lab2_1/lab2_1/Program.cs
+++ lab2_1/lab2_1/Program.cs
@@ -1,24 +1,21 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 
-namespace lab2_1
+namespace experiment
 {
-    internal class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Введите строку: ");
-            string str = Console.ReadLine();
-            Regex regex = new Regex(@"[А-Я]{1}");
-            MatchCollection matches = regex.Matches(str);
-            if (matches.Count > 0)
+            string s = Console.ReadLine();
+
+            Console.Write("Неанглийские заглавные буквы: ");
+            for (int i = 0; i < s.Length; i++)
             {
-                foreach (Match match in matches)
-                    Console.Write(match.Value + " ");
-            }
-            else
-            {
-                Console.WriteLine("Совпадений не найдено!");
+                if (s[i] >= 'А' && s[i] <= 'Я')
+                {
+                    Console.Write(s[i] + " ");
+                }
             }
         }
     }
